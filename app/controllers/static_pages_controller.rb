330f3375f5c33f80adf953272c2f3986a56e6a63
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-10 05:39:42 +0000

    Added home view's timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @simpletweet = current_user.simpletweets.build if logged_in?
+    if logged_in?
+      @simpletweet = current_user.simpletweets.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,8 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  
+  def feed_items
+    Simpletweet.where(user_id: following_user_ids + [self.id])
+  end
 end
